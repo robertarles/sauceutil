6b5c36be1999d03a381b0878fea6fbdbaa54ab6f
robertarles <robert@arles.us>
2019-05-06 07:21:16 -0700

    initial commit of cobraficated sauceutil.go

---
+++ cmd/uploads.go
@@ -0,0 +1,97 @@
+// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// uploadsCmd represents the uploads command
+var uploadsCmd = &cobra.Command{
+	Use:   "uploads",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("uploads called")
+	},
+}
+
+var apiURL = "https://saucelabs.com/rest/v1"
+
+func init() {
+	rootCmd.AddCommand(uploadsCmd)
+	uploads()
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// uploadsCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// uploadsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+type fileData struct {
+	Name  string  `json:"name"`
+	Size  uint32  `json:"size"`
+	Mtime float32 `json:"mtime"`
+	Md5   string  `json:"md5"`
+	Etag  string  `json:"etag"`
+}
+
+type storageResponse struct {
+	Files []fileData `json:"files"`
+}
+
+func uploads() {
+
+	username := os.Getenv("SAUCE_USERNAME")
+	accessKey := os.Getenv("SAUCE_ACCESS_KEY")
+
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", apiURL+"/storage/"+username, nil)
+	request.SetBasicAuth(username, accessKey)
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Printf("The http request failed with error %s\n", err)
+	} else {
+		respBody := storageResponse{}
+		data, _ := ioutil.ReadAll(response.Body)
+		json.Unmarshal(data, &respBody)
+
+		fmt.Println()
+		if len(respBody.Files) > 0 {
+			fmt.Println("Files: ")
+			for i := 0; i < len(respBody.Files); i++ {
+				fmt.Printf("%d) Name: %s, Size: %d\n", i+1, respBody.Files[i].Name, respBody.Files[i].Size)
+			}
+		} else {
+			fmt.Println("No files found.")
+		}
+	}
+
+}
