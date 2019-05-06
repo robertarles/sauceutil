c80af55e1b0f9970069c68cc8c3aa8901a529445
robertarles <robert@arles.us>
2019-05-06 09:20:11 -0700

    Working help, uploads, and upload commands

--- cmd/upload.go
+++ cmd/upload.go
@@ -15,18 +15,21 @@
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
 
-// uploadsCmd represents the uploads command
-var uploadsCmd = &cobra.Command{
-	Use:   "uploads",
+// uploadCmd represents the upload command
+var uploadCmd = &cobra.Command{
+	Use:   "upload",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -35,62 +38,71 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		uploads()
+		var filename string
+		if len(args) == 3 {
+			filename = args[2]
+		} else {
+			fmt.Printf("upload requires a filename parameter\ntry the --help option\n")
+			os.Exit(1)
+		}
+		upload(filename)
 	},
 }
 
-var apiURL = "https://saucelabs.com/rest/v1"
-
 func init() {
-	rootCmd.AddCommand(uploadsCmd)
+	rootCmd.AddCommand(uploadCmd)
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// uploadsCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// uploadCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// uploadsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// uploadCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-type fileData struct {
-	Name  string  `json:"name"`
-	Size  uint32  `json:"size"`
-	Mtime float32 `json:"mtime"`
-	Md5   string  `json:"md5"`
-	Etag  string  `json:"etag"`
+type uploadResponse struct {
+	Username string `json:"username"`
+	Filename string `json:"filename"`
+	Size     string `json:"size"`
+	Md5      string `json:"md5"`
+	Etag     string `json:"etag"`
 }
 
-type storageResponse struct {
-	Files []fileData `json:"files"`
-}
-
-func uploads() {
+// PostUpload Post a file to sauce-storage
+func upload(uploadFilepath string) {
 
 	username := os.Getenv("SAUCE_USERNAME")
 	accessKey := os.Getenv("SAUCE_ACCESS_KEY")
 
-	client := &http.Client{}
-	request, err := http.NewRequest("GET", apiURL+"/storage/"+username, nil)
+	uploadFilename := filepath.Base(uploadFilepath)
+	//open file and retrieve info
+	file, err := os.Open(uploadFilepath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fileContents, err := ioutil.ReadAll(file)
+	body := &bytes.Buffer{}
+	io.Copy(body, file)
+
+	postURL := apiURL + "/storage/" + username + "/" + uploadFilename + "?overwrite=true"
+
+	fmt.Printf("Posting to %s\n", postURL)
+	request, err := http.NewRequest("POST", postURL, body)
 	request.SetBasicAuth(username, accessKey)
+	request.Header.Add("Content-Type", "application/octet-stream")
+	client := &http.Client{}
 	response, err := client.Do(request)
 	if err != nil {
 		fmt.Printf("The http request failed with error %s\n", err)
+	} else if response.StatusCode != 200 {
+		fmt.Printf("Upload request failed with status code of %d\n", response.StatusCode)
 	} else {
-		respBody := storageResponse{}
+		respBody := uploadResponse{}
 		data, _ := ioutil.ReadAll(response.Body)
 		json.Unmarshal(data, &respBody)
-
-		fmt.Println()
-		if len(respBody.Files) > 0 {
-			fmt.Println("Files: ")
-			for i := 0; i < len(respBody.Files); i++ {
-				fmt.Printf("%d) Name: %s, Size: %d\n", i+1, respBody.Files[i].Name, respBody.Files[i].Size)
-			}
-		} else {
-			fmt.Println("No files found.")
-		}
+		fmt.Printf("\nUploaded - Username: %s, File: %s, Size: %s, md5: %s", respBody.Username, respBody.Filename, respBody.Size, respBody.Md5)
 	}
 
 }
